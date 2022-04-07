893201a78b578360214fcaaec0baeb6197a40dcd
shahadat13593 <shahadat15-13593@diu.edu.bd>
2022-04-07 18:58:56 +0600

    global

---
+++ 2 Lesson_basics/global.js
@@ -0,0 +1,25 @@
+
+
+// shahadat Hossain
+// id : 201-15-13593
+// global object
+
+console.log(global);
+
+global.setTimeout(() => {
+    console.log('In the timeout')
+    clearInterval(int);
+}, 3000);
+
+
+const int = setInterval(() => {
+    console.log('in the interval');
+    
+}, 1000);
+
+
+console.log(__dirname);
+console.log(__filename);
+
+
+console.log(document.querySelector);

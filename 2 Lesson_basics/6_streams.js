13ff43279ddc0901c0182700771a4846a38b9dcc
shahadat13593 <shahadat15-13593@diu.edu.bd>
2022-04-07 20:53:26 +0600

    streams

---
+++ 2 Lesson_basics/6_streams.js
@@ -0,0 +1,18 @@
+// shahadat hossain
+// id: 201-15-13593
+
+const fs = require('fs');
+
+
+const readStrem = fs.createReadStream('./docs/blog3.txt',{encoding: 'utf-8'});
+
+const writeStream = fs.createWriteStream('./docs/blog4.txt');
+
+readStrem.on('data', (chunk) =>{
+console.log('==================== new chunk ==================='); 
+console.log(chunk);
+
+writeStream.write('\nNew Chunk\n');
+writeStream.write(chunk);
+});
+
